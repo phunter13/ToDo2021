8093d30cfab04640b6c812dae7cf32e80fd9d0c0
Preston <preston.j.hunter@gmail.com>
2021-03-05 12:10:51 -0700

    initial commit
    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyALKh9y6oUdB4cd5wH6hV2FgAg14Zu07l4",
+  authDomain: "helloworld-f6193.firebaseapp.com",
+  databaseURL: "https://helloworld-f6193-default-rtdb.firebaseio.com",
+  projectId: "helloworld-f6193",
+  storageBucket: "helloworld-f6193.appspot.com",
+  messagingSenderId: "983590947139",
+  appId: "1:983590947139:web:5169395235233b746947f6"
 };
 
 firebase.initializeApp(config);
